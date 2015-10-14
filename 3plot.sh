464ec24a045444bbc98f6112446ebb85c3f96876
Vaagen <pcthrane@stud.ntnu.no>
2015-10-14 19:34:10 +0200

    Working tertiary, can predict single orbit as wall as calculates with dimensionless numbers

--- 3plot.sh
+++ 3plot.sh
@@ -1,3 +1,13 @@
 gnuplot << EOF
-plot 'tertiarypos1.txt' with line 'tertiarypos2.txt' title 'Position of two particles' with line
+
+a = 1.5 # range of axis
+
+set grid
+show grid
+set size square
+set xrange [-a:a]
+set yrange [-a:a]
+set xlabel 'Distance relative to starting radius of particle 1'
+#set ylabel 'Distance relative to starting radius of particle 1'
+plot '+' using (0):(0) title 'Central mass', 'tertiarypos1.txt' title 'Trajectory of particle 1' with line , 'tertiarypos2.txt' title 'Trajectory of particle 2' with line
 EOF
--- tertiary.py
+++ tertiary.py
@@ -1,37 +1,52 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-
-
 from math import *    #this command gives you acces to math functions, such as sin(), pow() etc
 
+# If you want to only analyze orbit of a single particle, set m2 = 0, and disregard files giving position, energy and angular momentum for particle 2.
+########################################
+G = 1               # Gravitational constant
+M = 2               # Central Mass
+m1 = 0.01           # mass of particle 1
+m2 = 0.01          # mass of particle 2
 
+x1 = 2              #initial position in x-direction, particle 1
+y1 =0               #initial position in y-direction, particle 1
+vx1 = 0             #initial velocity in x-direction, particle 1
+vy1 = 0.9           #initial velocity in y-direction, particle 1
 
-G = 1          # Gravitational constant
-M = 2         # Central Mass
-m1 = 0.0001    # mass of particle 1
-m2 = 0.0    # mass of particle 2
-dt = 0.0001   # integration timestep
+x2 = 0              #initial position in x-direction, particle 2
+y2 = 1              #initial position in y-direction, particle 2
+vx2 = 0.5           #initial velocity in x-direction, particle 2
+vy2 = 0             #initial velocity in y-direction, particle 2
 
-x1 = 1             #initial position in x-direction, particle 1
-y1 =0              #initial position in y-direction, particle 1
-vx1 = 0            #initial velocity in x-direction, particle 1
-vy1 = 1.5            #initial velocity in y-direction, particle 1
+dt = 0.0001          # integration timestep
+endtime=10         #total simulation time
 
-x2 = 1            #initial position in x-direction, particle 2
-y2 = 1             #initial position in y-direction, particle 2
-vx2 = 0            #initial velocity in x-direction, particle 2
-vy2 = 0            #initial velocity in y-direction, particle 2
+plotspacing = 1
+#########################################
+# Changing to dimensionless variables, using starting position and speed of particle 1 as referance
 
-time=0.0                 #this is the start time
-endtime=10        #total simulation time
+R01 = (x1**2 + y1**2)**0.5
+x1 = x1 / R01
+y1 = y1 / R01
+x2 = x2 / R01
+y2 = y2 / R01
+V01 = (vx1**2 + vy1**2)**0.5
+vx1 = vx1 / V01
+vy1 = vy1 / V01
+vx2 = vx2 / V01
+vy2 = vy2 / V01
 
-i = 1                # variable used to save only each <plotdensity>'th value...
-plotdensity = 10
+dt = dt * V01 / R01
+endtime = endtime * V01 / R01
 
-maxEnergyDeviation = 0              # maximum deviation of energy for a particle, relative to start
-maxAngularMomentumDeviation = 0     # maximum deviation of angular momentum for a particle, relative to start
+G = G / (R01 * V01**2)
 
+time=0.0                              #this is the start time
+maxEnergyDeviation = 0.0              # maximum deviation of energy for a particle, relative to start
+maxAngularMomentumDeviation = 0.0     # maximum deviation of angular momentum for a particle, relative to start
+i = 1                                 # variable used to save only each <plotspacing>'th value...
 
 # create files to save simulation data in:
 f = open('tertiarypos1.txt','w') # notice: the write option 'w' erases previous data in the file
@@ -40,8 +55,6 @@ f3 = open('tertiaryenergy1.txt','w')
 f4 = open('tertiaryenergy2.txt','w')
 f5 = open('tertiaryangularmomentum1.txt','w')
 f6 = open('tertiaryangularmomentum2.txt','w')
-
-
 
 def r1():    # distance between origin and particle 1
     return (x1**2 + y1**2)**0.5
@@ -99,7 +112,7 @@ while (time < endtime):
         vx2 = vxm2 + 0.5*dt*fx2 # calculating new speed, particle 2
         vy2 = vym2 + 0.5*dt*fy2
 
-        if (i % plotdensity == 0):  # writing to files only each <plotdensity>'th iteration
+        if (i % plotspacing == 0):  # writing to files only each <plotspacing>'th iteration
             E1 = 0.5*m1*(vx1**2 + vy1**2) - G*M*m1/r1() - G*m1*m2/r1r2() # total energy of particle 1
             E2 = 0.5*m2*(vx2**2 + vy2**2) - G*M*m2/r2() - G*m1*m2/r1r2() # total energy of particle 2
             
@@ -113,13 +126,13 @@ while (time < endtime):
             f5.write("%f %f\n" % (time, L1))
             f6.write("%f %f\n" % (time, L2))
 
-            if E01 > 0 and (abs(1-E1/E01) > maxEnergyDeviation):
+            if abs(E01) > 0 and (abs(1-E1/E01) > maxEnergyDeviation):
                     maxEnergyDeviation = 1 - E1/E01
-            if E02 > 0 and (abs(1-E2/E02) > maxEnergyDeviation):
+            if abs(E02) > 0 and (abs(1-E2/E02) > maxEnergyDeviation):
                     maxEnergyDeviation = 1 - E2/E02
-            if L01 > 0 and (abs(1-L1/L01) > maxAngularMomentumDeviation):
+            if abs(L01) > 0 and (abs(1-L1/L01) > maxAngularMomentumDeviation):
                     maxAngularMomentumDeviation = L1/L01 - 1
-            if L02 > 0 and (abs(1-L2/L02) > maxAngularMomentumDeviation):
+            if abs(L02) > 0 and (abs(1-L2/L02) > maxAngularMomentumDeviation):
                     maxAngularMomentumDeviation = L2/L02 - 1
 
 print("Maximum deviation of energy for a particle relative to start energy is %f\n" % maxEnergyDeviation)
